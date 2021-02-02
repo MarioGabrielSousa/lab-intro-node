50c1626bfda2d732b001d5ec1b55fd285d0f0385
mario sousa <vegetalizado@gmail.com>
2021-02-02 14:07:05 +0000

    done

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [],
+      this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a, b) => {
+      return a - b;
+    })
+    return this.items;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items.length < 0 || pos > this.items.length) {
+      throw new Error("OutofBounds")
+    }
+    return this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.items[this.items.length - 1]
+  }
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList")
+    }
+    return this.items[0]
+  }
 
-  sum() {}
+  sum() {
+    
+  }
 
   avg() {}
 }
